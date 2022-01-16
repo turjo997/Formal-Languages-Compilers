34b07634164f9162da30e04a73a7167d7969bd3e
Ullash Bhattacharjee(Turjo) <53423899+turjo997@users.noreply.github.com>
2022-01-16 19:57:41 +0600

    Add files via upload

---
+++ Assignment3/input1.c
@@ -0,0 +1,15 @@
+// A program fragment
+float x1 = 3.125;
+/* Definition of the
+function f1 */
+double f1(int x)
+{
+double z;
+z = 0.01;
+return z;
+}
+//* Beginning of 'main'
+int main(void)
+{
+int n1; double z;
+n1=25;
